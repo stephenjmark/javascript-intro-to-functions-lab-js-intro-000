0fcf386206327f7160fc1b18cf9debe1279ce293
Stephen Mark <stephenjmark@gmail.com>
2017-11-07 03:42:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,9 +7,11 @@ function whisper(string) {
 }
 
 function logShout(string) {
-  console.log(string.toUpperCase())()
+  var shout = string.toUpperCase()
+  console.log(shout)
 }
 
-function logShout(string) {
-  console.log(string.toLowerCase())()
+function logWhisper(string) {
+  var whisper = string.toLowerCase()
+  console.log(whisper)
 }
