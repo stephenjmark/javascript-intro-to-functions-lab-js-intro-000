fb4f2dffce1b1bdfba902b1ae85287b936f99bfe
Stephen Mark <stephenjmark@gmail.com>
2017-11-07 03:51:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,9 +17,12 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string === string.toUpperCase()){
+  var shout = string.toUpperCase()
+  var whisper = string.toLowerCase()
+  
+  if (string === shout){
     return "YES INDEED!"
-  } else if (string === string.tolowerCase()){
+  } else if (string === whisper){
     return "I can't hear you!"
   } else if (string === "I love you, Grandma." ){
     return "I love you, too."
